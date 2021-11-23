2905b9001d47cef4343773301fc8ab60db2e1652
Sanjay2109 <83913295+Sanjay2109@users.noreply.github.com>
2021-11-23 15:08:06 +0530

    Weather app update 1
    Weather app update (new background, latitude and longitude are visible)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React, { useState, Image } from "react";
+import React, { useState } from "react";
 const api = {
   key: "a033095717e3d13b07057f3725502834",
   base: "https://api.openweathermap.org/data/2.5/",
 };
-const imageUrl = "http://openweathermap.org/img/wn/";
+
 function App() {
   const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
 
-  const search = (evt) => {
-    if (evt.key === "Enter") {
+  const search = (event) => {
+    if (event.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
         .then((res) => res.json())
         .then((result) => {
-          setWeather(result);
           setQuery("");
+          console.log("weather result: ");
           console.log(result);
+          fetch(
+            `https://api.opencagedata.com/geocode/v1/json?q=${query}&key=000594ff6b2744c59c5381e9948cef87 `
+          )
+            .then((res) => res.json())
+            .then((result2) => {
+              console.log("Result2: ");
+              console.log(result2["results"][0]["geometry"]["lat"]);
+              console.log(result2["results"][0]["geometry"]["lng"]);
+              result["weatherLatLon"] = result2["results"][0]["geometry"];
+              console.log(result["weatherLatLon"]);
+              setWeather(result);
+            });
         });
     }
   };
@@ -68,7 +80,7 @@ function App() {
           <input
             type="text"
             className="search-bar"
-            placeholder="Search for a state"
+            placeholder="Search for a state...."
             onChange={(e) => setQuery(e.target.value)}
             value={query}
             onKeyPress={search}
@@ -78,30 +90,35 @@ function App() {
           <div>
             <div className="location-box">
               <div className="location">
-                {weather.name}, {weather.sys.country},
+                {weather.name}, {weather.sys.country}
               </div>
               <div className="date">{dateBuilder(new Date())}</div>
             </div>
             <div className="weather-box">
               <div className="temp">{Math.round(weather.main.temp)}°c</div>
               <div className="weather">
-                Main: {weather.weather[0].main}{" "}
+                Latitude: {weather.weatherLatLon.lat}°
+              </div>
+              <div className="weather">
+                Longitude: {weather.weatherLatLon.lng}°
+              </div>
+              <div className="weather">
+                Sky: {weather.weather[0]["description"]}
+              </div>
+              <div className="weather">
                 <img
-                  src={imageUrl + weather.weather[0].icon + ".png"}
+                  src={"https://cdn-icons-png.flaticon.com/128/632/632517.png"}
+                  width="50"
+                  height="60"
                   alt="Logo"
                 />
-              </div>
-              <div className="weather">
-                Description: {weather.weather[0].description}
-              </div>
-              <div className="weather">
                 Pressure: {weather.main.pressure} Pa
               </div>
               <div className="weather">Humidity: {weather.main.humidity} %</div>
             </div>
           </div>
         ) : (
-          ""
+          <div></div>
         )}
       </main>
     </div>
